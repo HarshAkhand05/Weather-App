43ce716a5f5702ce7f1e30c22f3d0367708f9877
HarshAkhand05 <harshakhand998@gmail.com>
2024-03-31 15:35:24 +0530

    added some new  function and  solve some error in code

--- weather.js
+++ weather.js
@@ -8,9 +8,9 @@ const userinfo = document.querySelector(".user-info")
 
 // variable
 let currenttab = usertab;
-const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+const API_KEY = "168771779c71f3d64106d8a88376808a";
 currenttab.classList.add("current-tab")
-
+ getfromsessionstorage();
 
 function switchtab(clicked) {
     if (clicked != currenttab) {
@@ -57,8 +57,9 @@ async function fetchuserweatherinfo(coordinates) {
     grantacessloc.classList.remove("active")
     loadingscreen.classList.add("active")
     try {
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+        const res =await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
         const data = await res.json();
+    
         loadingscreen.classList.remove("active");
         userinfo.classList.add("active");
         renderweatherinfo(data);
@@ -69,7 +70,7 @@ async function fetchuserweatherinfo(coordinates) {
 
 }
 function renderweatherinfo(weatherinfo) {
-    const cityname = document.querySelector("[data-city>]")
+    const cityname = document.querySelector("[data-city]")
     const country = document.querySelector("[data-contry-icon]")
     const citydisc = document.querySelector("[data-discrp]")
     const weathericon = document.querySelector("[weather-icon]")
@@ -84,10 +85,10 @@ function renderweatherinfo(weatherinfo) {
     country.src = `https://flagcdn.com/144x108/${weatherinfo?.sys?.country.toLowerCase()}.png`;
     citydisc.innerText = weatherinfo?.weather?.[0]?.description;
     weathericon.src = `http://openweathermap.org/img/w/${weatherinfo?.weather?.[0]?.icon}.png`
-    temp.innerText = weatherinfo?.main?.temp;
-    windspeed.innerText = weatherinfo?.wind?.speed;
-    humidity.innerText = weatherinfo?.main?.humidity;
-    cloudinfo.innerText = weatherinfo?.clouds?.all;
+    temp.innerText = `${weatherinfo?.main?.temp} °C`;
+    windspeed.innerText =`${ weatherinfo?.wind?.speed}  m/s`;
+    humidity.innerText = `${weatherinfo?.main?.humidity}%`;
+    cloudinfo.innerText =`${ weatherinfo?.clouds?.all}%`;
 }
 
 
@@ -116,3 +117,36 @@ function showPosition(position) {
 
 const grantacessbutton = document.querySelector("[data-access]")
 grantacessbutton.addEventListener('click', getuserloc)
+
+
+const searchinpt= document.querySelector("[data-searchinput]")
+searchbar.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let citynaam= searchinpt.value;
+    console.log(citynaam);
+    if(citynaam==="")
+    {
+        return;
+    }
+    else{
+        fetchsearchweatherinfo(citynaam);
+    }
+
+})
+
+
+async function fetchsearchweatherinfo(citynaam)
+{
+    loadingscreen.classList.add("active");
+    userinfo.classList.remove("active");
+    grantacessloc.classList.remove("active");
+    try {
+        const response1 =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citynaam}&appid=${API_KEY}&units=metric`);
+        const data = await response1.json();
+        console.log("city",data);
+        loadingscreen.classList.remove("active");
+        renderweatherinfo(data);
+    } catch (err) {
+        
+    }
+}
